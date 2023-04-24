fe4f8846640d8288965382fcc5478090ca05bdb7
kkoorrbbiinn <korbin.westerfield.kw@gmail.com>
2023-04-24 11:29:51 -0700

    broken commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: path.resolve(__dirname, 'frontend'),
+  envFile: path.resolve(__dirname, '.env'),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
+  }
+})
