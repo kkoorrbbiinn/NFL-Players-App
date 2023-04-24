0374ea5337f9c89640f2aaf20d9e66415e217cd8
kkoorrbbiinn <korbin.westerfield.kw@gmail.com>
2023-04-24 11:36:11 -0700

    both run commands working

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 // const path = require('path')
-const connectToDb = require('./Backend/models/index');
+const connectToDb = require('./models/index');
 // const commentsCtrl = require('./controllers/comments')
 const app = express();
 
 connectToDb();
 
-app.get('/api/comments', require('./Backend/routes/commentRoutes'))
+app.get('/api/comments', require('./routes/commentRoutes'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use('/api/comments', require('./Backend/routes/commentRoutes'))
+app.use('/api/comments', require('./routes/commentRoutes'))
 
 app.listen(process.env.PORT, function () {
     console.log('Express is listening to port', process.env.PORT);
