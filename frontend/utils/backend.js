2d46fbb84657fdf3cba548fc906074e021b7a869
kkoorrbbiinn <korbin.westerfield.kw@gmail.com>
2023-05-04 21:24:57 -0700

    fixed routes

--- frontend/utils/backend.js
+++ frontend/utils/backend.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export async function getComments(playerId) {
-    const { data } = await axios.get(`http://localhost:3000/api/comments/${playerId}`)
+    const { data } = await axios.get(`/api/comments/${playerId}`)
     return data
 }
 
 export async function postComments(comment) {
-    const { data } = await axios.post('http://localhost:3000/api/comments', comment)
+    const { data } = await axios.post('/api/comments', comment)
     return data
 }
 
 export async function putComments(comment) {
-    const { data } = await axios.put(`http://localhost:3000/api/comments`, comment)
+    const { data } = await axios.put(`/api/comments`, comment)
     return data
 }
 
 export async function deleteComments(commentId) {
-    const { data } = await axios.delete(`http://localhost:3000/api/comments/${commentId}`)
+    const { data } = await axios.delete(`/api/comments/${commentId}`)
     return data
 }
